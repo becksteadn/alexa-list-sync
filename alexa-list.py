26af0cab2a35bfeb55f160ce2195ac9e4c31099f
Nathaniel <nathaniel@scriptingis.life>
2021-06-22 22:09:11 -0400

    basic airtable API

--- alexa-list.py
+++ alexa-list.py
@@ -1,12 +1,18 @@
 import json
+from helper import Airtable
 
 def item_added(event, context):
     item = json.loads(event["body"])["item"]
     
+    airtable = Airtable()
+    print(airtable.list_all_records())
+
     body = {
         "message": f"The item {item} was added!",
         "item": item
     }
+
+    print(body)
 
     response = {
         "statusCode": 200,
@@ -23,11 +29,11 @@ def item_removed(event, context):
         "item": item
     }
 
+    print(body)
+
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
-
-    print(response)
 
     return response
