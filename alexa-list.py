64cd0be8112f078936322e326312a88e97f265ac
Nathaniel <nathaniel@scriptingis.life>
2021-06-23 16:16:32 -0400

    sync alexa and airtable

--- alexa-list.py
+++ alexa-list.py
@@ -1,15 +1,96 @@
 import json
 from helper import Airtable
 
+def get_grocery_list():
+    """Get a list of grocery items
+
+    Fetch Airtable data and turn it into a basic list of items
+
+    Returns
+    -------
+    list
+        A list of grocery items
+    """
+    airtable = Airtable()
+    records = airtable.list_all_records(airtable.listView)["records"]
+    grocery_list = records_to_list(records)
+    return grocery_list
+
+def records_to_list(records):
+    """Change record JSON to list of Name
+
+    Parameters
+    ----------
+    records : dict
+        JSON data returned from Airtable
+
+    Returns
+    -------
+    list
+        A list of Name values
+    """
+    grocery_list = list()
+    for item in records:
+        grocery_list.append(item["fields"]["Name"])
+    return grocery_list
+
+def update_table(item, add_item):
+    """Update a record in the table
+
+    If the record does not exist, create one and add it to
+    the grocery list. If it does exist, update its
+    checkbox. When an item is added, the checkbox will be checked.
+    When an item is removed, the checkbox will be unchecked.
+
+    Parameters
+    ----------
+    item : str
+        The name of the grocery item
+    add_item : bool
+        True if the item is being added, False if it is being removed
+
+    Returns
+    -------
+    dict
+        JSON data response from Airtable
+    """
+    airtable = Airtable()
+    all_items = airtable.list_all_names(airtable.allView)
+    print("Number of Grocery Items: " + str(len(all_items)))
+    if item in all_items or add_item == False:
+        response = airtable.update_record(item, ("Shopping List", add_item))
+    else:
+        response = airtable.create_record(item)
+    return response
+
+
 def item_added(event, context):
+    """Lambda handler for grocery item being added
+
+    Parameters
+    ----------
+    event : dict
+        Event data
+    context : dict
+        Context data
+
+    Returns
+    -------
+    dict
+        JSON HTTP response
+    """
     item = json.loads(event["body"])["item"]
     
-    airtable = Airtable()
-    print(airtable.list_all_records())
+    grocery_list = get_grocery_list()
+
+    item_added = update_table(item, True)
+    print(item_added)
 
     body = {
         "message": f"The item {item} was added!",
-        "item": item
+        "item": item,
+        "list": grocery_list,
+        "item_added": item_added
     }
 
     print(body)
@@ -22,11 +103,29 @@ def item_added(event, context):
     return response
 
 def item_removed(event, context):
+    """Lambda handler for grocery item being removed
+
+    Parameters
+    ----------
+    event : dict
+        Event data
+    context : dict
+        Context data
+
+    Returns
+    -------
+    dict
+        JSON HTTP response
+    """
     item = json.loads(event["body"])["item"]
+
+    item_removed = update_table(item, False)
+    print(item_removed)
 
     body = {
         "message": f"The item {item} was removed!",
-        "item": item
+        "item": item,
+        "item_removed": item_removed
     }
 
     print(body)
--- helper.py
+++ helper.py
@@ -3,25 +3,165 @@ import boto3
 import requests
 
 class Airtable:
+    """
+    A class to interact with the Airtable API.
+    """
+
     def __init__(self):
+        """
+        Parameters are retrieved from system environment variables.
+        """
         self.apikey = self.get_api_key(os.environ["paramNameAirtable"])
         self.baseId = os.environ["airtableBaseId"]
         self.table = os.environ["airtableTableName"]
+        self.listView = os.environ["airtableListView"]
+        self.allView = os.environ["airtableAllView"]
+        self.url = "https://api.airtable.com/v0/" + self.baseId + "/" + self.table
+        self.headers = {
+            "Authorization": "Bearer " + self.apikey
+        }
 
     def get_api_key(self, parameter_name):
+        """Retrieve the API key from AWS Systems Manager Parameter Store
+
+        Parameters
+        ----------
+        parameter_name : str
+            Name of the parameter in AWS
+
+        Returns
+        -------
+        str
+            The value of the parameter
+        """
         ssm = boto3.client('ssm')
         parameter = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
         return parameter['Parameter']['Value']
 
-    def list_all_records(self, table=None):
-        if table is None:
-            table = self.table
-        url = "https://api.airtable.com/v0/" + self.baseId + "/" + table
+    def get_record_id_by_name(self, name):
+        """Input an item name and get the Airtable record ID
+
+        Parameters
+        ----------
+        name : str
+            Name of the item
+
+        Returns
+        -------
+        str
+            Airtable ID of the record
+        """
+        all_records = self.list_all_records(self.allView)
+        for record in all_records["records"]:
+            if record["fields"]["Name"] == name:
+                return record["id"]
+        return None
+
+    def create_record(self, name):
+        """Make the API request to create a new record in a table
+
+        Forces Shopping List = True since a record will only be
+        created when a new item is added.
+
+        Parameters
+        ----------
+        name : str
+            The name of the item
+
+        Returns
+        -------
+        dict
+            JSON data from the Airtable API response
+        """
+        headers = self.headers
+        headers["Content-Type"] = "application/json"
+        data = {
+            "records": [
+                {
+                    "fields": {
+                        "Name": name,
+                        "Shopping List": True
+                    }
+                }
+            ]
+        }
+        r = requests.post(self.url, headers=headers, json=data)
+        return r.json()
+
+    def update_record(self, name, field):
+        """Make the API request to update a single field of a record
+
+        Calls get_record_id_by_name() then updates the record with a
+        specified field and value.
+
+        Parameters
+        ----------
+        name : str
+            The name of the item
+        field : tuple
+            A tuple including the field name and value
+
+        Returns
+        -------
+        dict
+            JSON data from the Airtable API response
+
+        """
+        headers = self.headers
+        headers["Content-Type"] = "application/json"
+        data = {
+            "records": [
+                {
+                    "id": self.get_record_id_by_name(name),
+                    "fields": {
+                        field[0]: field[1]
+                    }
+                }
+            ]
+        }
+        r = requests.patch(self.url, headers=headers, json=data)
+        return r.json()
+
+    def list_all_names(self, view):
+        """List all Name values from a specified view
+
+        Parameters
+        ----------
+        view : str
+            The Airtable View to use
+
+        Returns
+        -------
+        list
+            A list of Name values for each item in a view
+
+        """
         params = {
-            "view": "Shopping List"
+            "view": view
         }
-        headers = {
-            "Authorization": "Bearer " + self.apikey
+        r = requests.get(self.url, params=params, headers=self.headers)
+        records = r.json()["records"]
+        items_list = list()
+        for item in records:
+            items_list.append(item["fields"]["Name"])
+        return items_list
+
+    def list_all_records(self, view):
+        """Return Airtable data from a specified view
+
+        Parameters
+        ----------
+        view : str
+            The Airtable View to use
+
+        Returns
+        -------
+        dict
+            JSON data from the Airtable API response
+
+        """
+        params = {
+            "view": view
         }
-        r = requests.get(url, params=params, headers=headers)
+        r = requests.get(self.url, params=params, headers=self.headers)
         return r.json()
